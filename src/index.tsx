3b424102236c887dada9f717a42096ecaac628c6
イヨンウ <lee.yong.woo@arsaga.jp>
2023-11-24 18:07:25 +0900

    add file system hook

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import { DefaultType, Item } from './types/types';
 
 export { FileSystemContainer } from './FileSystemContainer';
 export { FileSystemProvider } from './context/FileSystemContext';
+export { useFileSystem } from './hooks/useFileSystem';
 
 export type FileElement<T extends DefaultType> = Item<T>;
